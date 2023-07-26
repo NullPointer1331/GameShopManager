b4462c1fcc7b34ac0555a4d76d653d7f51914a26
NullPointer1331 <130084942+NullPointer1331@users.noreply.github.com>
2023-07-26 14:47:15 -0700

    Added documentation to User class

--- GameShopManager/User.cs
+++ GameShopManager/User.cs
@@ -8,18 +8,42 @@ namespace GameShopManager
 {
     internal class User
     {
+        /// <summary>
+        /// Reference ID for database
+        /// </summary>
         public int UserId { get; set; }
+        /// <summary>
+        /// Display name for the user account
+        /// </summary>
         public required string UserName { get; set; }
+        /// <summary>
+        /// Password for the user account
+        /// </summary>
         public required string Password { get; set; }
+        /// <summary>
+        /// Amount of cash the user has, can be used to purchase items
+        /// </summary>
         public int Cash { get; set; }
         public Item[] Inventory { get; set; }
 
+        /// <summary>
+        /// Creates a new user with a username and password, and sets cash to 0
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="password"></param>
         public User(string userName, string password)
         {
             UserName = userName;
             Password = password;
             Cash = 0;
         }
+
+        /// <summary>
+        /// Creates a new user with a username, password, and cash
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="password"></param>
+        /// <param name="cash"></param>
         public User(string userName, string password, int cash)
         {
             UserName = userName;
