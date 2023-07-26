843601b6aaa435cf8d1631454cf35108fb751c03
TortillaWrappedParrot <119008633+TortillaWrappedParrot@users.noreply.github.com>
2023-07-26 14:33:36 -0700

    Added ItemObject class layout according to DB structure

---
+++ GameShopManager/ItemObject.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameShopManager
+{
+    internal class ItemObject
+    {
+        /// <summary>
+        /// Refrence ID for database
+        /// </summary>
+        public int ItemID { get; set; }
+        
+        /// <summary>
+        /// Display name for user interface
+        /// </summary>
+        public string ItemName { get; set; }
+
+        /// <summary>
+        /// Displayed description for item
+        /// </summary>
+        public string ItemDescription { get; set; }
+
+        /// <summary>
+        /// Current price for item
+        /// </summary>
+        public int ItemPrice { get; set; }
+
+        /// <summary>
+        /// Amount of item stored
+        /// </summary>
+        public int Quantity { get; set; }
+    }
+}
