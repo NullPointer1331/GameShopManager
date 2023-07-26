0ddd0babe0eff70d7adc91fce630ced8275900aa
NullPointer1331 <130084942+NullPointer1331@users.noreply.github.com>
2023-07-26 14:42:39 -0700

    Basic implementation of User class

---
+++ GameShopManager/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameShopManager
+{
+    internal class User
+    {
+        public int UserId { get; set; }
+        public required string UserName { get; set; }
+        public required string Password { get; set; }
+        public int Cash { get; set; }
+        public Item[] Inventory { get; set; }
+
+        public User(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+            Cash = 0;
+        }
+        public User(string userName, string password, int cash)
+        {
+            UserName = userName;
+            Password = password;
+            Cash = cash;
+        }
+    }
+}
