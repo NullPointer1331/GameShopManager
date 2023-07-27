ce1b41df46af461951e419214ee0746f8ce48121
NullPointer1331 <130084942+NullPointer1331@users.noreply.github.com>
2023-07-27 11:56:13 -0700

    Added EntityFrameworkCore.Tools package

--- GameShopManager/UserObject.cs
+++ GameShopManager/UserObject.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ namespace GameShopManager
         /// <summary>
         /// Reference ID for database
         /// </summary>
+        [Key]
         public int UserID { get; set; }
 
         /// <summary>
