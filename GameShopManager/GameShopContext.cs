c4e3400e7613883c12638d7a160cf8fc682b1039
NullPointer1331 <130084942+NullPointer1331@users.noreply.github.com>
2023-07-27 11:51:58 -0700

    Added context class and set up configuration

---
+++ GameShopManager/GameShopContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameShopManager
+{
+    internal class GameShopContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=GameShopManager;Trusted_Connection=True;");
+        }
+    }
+}
