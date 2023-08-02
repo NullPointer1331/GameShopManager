c5c3682652f65c4a4b4472e1c41e97f7adbc2069
TortillaWrappedParrot <119008633+TortillaWrappedParrot@users.noreply.github.com>
2023-08-02 15:57:18 -0700

    Created navigation buttons for quick testing

--- GameShopManager/Forms/NavigationForm.Designer.cs
+++ GameShopManager/Forms/NavigationForm.Designer.cs
@@ -28,134 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
-            button5 = new Button();
-            button6 = new Button();
-            button7 = new Button();
-            this.label1 = new Label();
-            this.label2 = new Label();
-            this.label3 = new Label();
-            this.label4 = new Label();
+            LoginUser = new Button();
+            CreateUser = new Button();
+            AddItem = new Button();
+            DeleteItem = new Button();
+            InventoryDisplay = new Button();
+            AddItemQuantity = new Button();
+            DeleteUser = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
             SuspendLayout();
             // 
-            // button1
+            // LoginUser
             // 
-            button1.Location = new Point(12, 61);
-            button1.Name = "button1";
-            button1.Size = new Size(100, 50);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            LoginUser.Location = new Point(12, 61);
+            LoginUser.Name = "LoginUser";
+            LoginUser.Size = new Size(100, 50);
+            LoginUser.TabIndex = 0;
+            LoginUser.Text = "Show Login Form";
+            LoginUser.UseVisualStyleBackColor = true;
+            LoginUser.Click += LoginUser_Click;
             // 
-            // button2
+            // CreateUser
             // 
-            button2.Location = new Point(12, 128);
-            button2.Name = "button2";
-            button2.Size = new Size(100, 50);
-            button2.TabIndex = 1;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
+            CreateUser.Location = new Point(12, 128);
+            CreateUser.Name = "CreateUser";
+            CreateUser.Size = new Size(100, 50);
+            CreateUser.TabIndex = 1;
+            CreateUser.Text = "Show Create Login Form";
+            CreateUser.UseVisualStyleBackColor = true;
+            CreateUser.Click += CreateUser_Click;
             // 
-            // button3
+            // AddItem
             // 
-            button3.Location = new Point(211, 61);
-            button3.Name = "button3";
-            button3.Size = new Size(100, 50);
-            button3.TabIndex = 2;
-            button3.Text = "button3";
-            button3.UseVisualStyleBackColor = true;
+            AddItem.Location = new Point(217, 61);
+            AddItem.Name = "AddItem";
+            AddItem.Size = new Size(100, 50);
+            AddItem.TabIndex = 2;
+            AddItem.Text = "Show Create Item Form";
+            AddItem.UseVisualStyleBackColor = true;
+            AddItem.Click += AddItem_Click;
             // 
-            // button4
+            // DeleteItem
             // 
-            button4.Location = new Point(211, 128);
-            button4.Name = "button4";
-            button4.Size = new Size(100, 50);
-            button4.TabIndex = 3;
-            button4.Text = "button4";
-            button4.UseVisualStyleBackColor = true;
+            DeleteItem.Location = new Point(217, 198);
+            DeleteItem.Name = "DeleteItem";
+            DeleteItem.Size = new Size(100, 50);
+            DeleteItem.TabIndex = 3;
+            DeleteItem.Text = "Show Delete Item Form";
+            DeleteItem.UseVisualStyleBackColor = true;
+            DeleteItem.Click += DeleteItem_Click;
             // 
-            // button5
+            // InventoryDisplay
             // 
-            button5.Location = new Point(432, 61);
-            button5.Name = "button5";
-            button5.Size = new Size(100, 50);
-            button5.TabIndex = 4;
-            button5.Text = "button5";
-            button5.UseVisualStyleBackColor = true;
+            InventoryDisplay.Location = new Point(432, 61);
+            InventoryDisplay.Name = "InventoryDisplay";
+            InventoryDisplay.Size = new Size(100, 50);
+            InventoryDisplay.TabIndex = 4;
+            InventoryDisplay.Text = "Show Inventory Form";
+            InventoryDisplay.UseVisualStyleBackColor = true;
+            InventoryDisplay.Click += InventoryDisplay_Click;
             // 
-            // button6
+            // AddItemQuantity
             // 
-            button6.Location = new Point(432, 128);
-            button6.Name = "button6";
-            button6.Size = new Size(100, 50);
-            button6.TabIndex = 5;
-            button6.Text = "button6";
-            button6.UseVisualStyleBackColor = true;
+            AddItemQuantity.Location = new Point(217, 128);
+            AddItemQuantity.Name = "AddItemQuantity";
+            AddItemQuantity.Size = new Size(100, 50);
+            AddItemQuantity.TabIndex = 5;
+            AddItemQuantity.Text = "Show Add Quantity Form";
+            AddItemQuantity.UseVisualStyleBackColor = true;
+            AddItemQuantity.Click += AddItemQuantity_Click;
             // 
-            // button7
+            // DeleteUser
             // 
-            button7.Location = new Point(642, 61);
-            button7.Name = "button7";
-            button7.Size = new Size(100, 50);
-            button7.TabIndex = 6;
-            button7.Text = "button7";
-            button7.UseVisualStyleBackColor = true;
+            DeleteUser.Location = new Point(12, 198);
+            DeleteUser.Name = "DeleteUser";
+            DeleteUser.Size = new Size(100, 50);
+            DeleteUser.TabIndex = 6;
+            DeleteUser.Text = "Delete Active User";
+            DeleteUser.UseVisualStyleBackColor = true;
+            DeleteUser.Click += DeleteUser_Click;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new Point(52, 26);
-            this.label1.Name = "label1";
-            this.label1.Size = new Size(38, 15);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "label1";
+            label1.AutoSize = true;
+            label1.Location = new Point(24, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(73, 15);
+            label1.TabIndex = 7;
+            label1.Text = "Login Forms";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new Point(217, 26);
-            this.label2.Name = "label2";
-            this.label2.Size = new Size(38, 15);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "label2";
+            label2.AutoSize = true;
+            label2.Location = new Point(232, 26);
+            label2.Name = "label2";
+            label2.Size = new Size(74, 15);
+            label2.TabIndex = 8;
+            label2.Text = "Item Actions";
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new Point(442, 22);
-            this.label3.Name = "label3";
-            this.label3.Size = new Size(38, 15);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "label3";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new Point(652, 21);
-            this.label4.Name = "label4";
-            this.label4.Size = new Size(38, 15);
-            this.label4.TabIndex = 10;
-            this.label4.Text = "label4";
+            label3.AutoSize = true;
+            label3.Location = new Point(446, 26);
+            label3.Name = "label3";
+            label3.Size = new Size(73, 15);
+            label3.TabIndex = 9;
+            label3.Text = "User Actions";
             // 
             // NavigationForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(this.label4);
-            Controls.Add(this.label3);
-            Controls.Add(this.label2);
-            Controls.Add(this.label1);
-            Controls.Add(button7);
-            Controls.Add(button6);
-            Controls.Add(button5);
-            Controls.Add(button4);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(button1);
+            ClientSize = new Size(561, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(DeleteUser);
+            Controls.Add(AddItemQuantity);
+            Controls.Add(InventoryDisplay);
+            Controls.Add(DeleteItem);
+            Controls.Add(AddItem);
+            Controls.Add(CreateUser);
+            Controls.Add(LoginUser);
             Name = "NavigationForm";
             Text = "NavigationForm";
             ResumeLayout(false);
@@ -164,12 +160,15 @@
 
         #endregion
 
-        private Button button1;
-        private Button button2;
-        private Button button3;
-        private Button button4;
-        private Button button5;
-        private Button button6;
-        private Button button7;
+        private Button LoginUser;
+        private Button CreateUser;
+        private Button AddItem;
+        private Button DeleteItem;
+        private Button InventoryDisplay;
+        private Button AddItemQuantity;
+        private Button DeleteUser;
+        private Label label1;
+        private Label label2;
+        private Label label3;
     }
 }
--- GameShopManager/Forms/NavigationForm.cs
+++ GameShopManager/Forms/NavigationForm.cs
@@ -16,5 +16,51 @@ namespace GameShopManager.Forms
         {
             InitializeComponent();
         }
+
+        private void InventoryDisplay_Click(object sender, EventArgs e)
+        {
+            InventoryUserForm form = new InventoryUserForm();
+            form.Show();
+        }
+
+        private void AddItem_Click(object sender, EventArgs e)
+        {
+            CreateItemForm form = new CreateItemForm();
+            form.Show();
+        }
+
+        private void AddItemQuantity_Click(object sender, EventArgs e)
+        {
+            AddItemForm form = new AddItemForm();
+            form.Show();
+        }
+
+        private void DeleteItem_Click(object sender, EventArgs e)
+        {
+            DeleteItemForm form = new DeleteItemForm();
+            form.Show();
+        }
+
+        private void LoginUser_Click(object sender, EventArgs e)
+        {
+            LoginUserForm form = new LoginUserForm();
+            form.Show();
+        }
+
+        private void CreateUser_Click(object sender, EventArgs e)
+        {
+            RegistrationUserForm form = new RegistrationUserForm();
+            form.Show();
+        }
+
+        private void DeleteUser_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to do this?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (dialogResult == DialogResult.Yes)
+            {
+                //delete active object
+                throw new NotImplementedException();
+            }
+        }
     }
 }
