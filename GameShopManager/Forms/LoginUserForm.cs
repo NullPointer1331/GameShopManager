732a7b202816a922a4269db64bec254aa3586b17
TortillaWrappedParrot <119008633+TortillaWrappedParrot@users.noreply.github.com>
2023-08-02 16:51:39 -0700

    Created login screen

--- GameShopManager/Forms/LoginUserForm.cs
+++ GameShopManager/Forms/LoginUserForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,53 @@ namespace GameShopManager.Forms
         public LoginUserForm()
         {
             InitializeComponent();
+        }
+
+        List<UserObject>? users;
+
+        private void LoginUserForm_Load(object sender, EventArgs e)
+        {
+            users = UserDB.GetAllUsers();
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            string user = UsernameInput.Text.Trim();
+            string password = PasswordInput.Text.Trim();
+            if (!string.IsNullOrEmpty(user))
+            {
+                if (!string.IsNullOrEmpty(password))
+                {
+                    if (users == null)
+                    {
+                        //Edge case if db is empty
+                        return;
+                    }
+                    foreach (UserObject item in users)
+                    {
+                        if (item.UserName == user && item.Password == password)
+                        {
+                            NavigationForm form = new NavigationForm();
+                            form.Show();
+                        }
+                    }
+                }
+                else
+                {
+                    PasswordError.Text = "Password is empty";
+                }
+            }
+            else
+            {
+                UserError.Text = "Username is empty";
+            }
+        }
+
+
+        private void RegisterAccount_Click(object sender, EventArgs e)
+        {
+            RegistrationUserForm form = new RegistrationUserForm();
+            form.ShowDialog();
         }
     }
 }
