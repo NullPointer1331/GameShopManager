47a1cb7476180995479b8d3a36e1f42152fc6734
NullPointer1331 <130084942+NullPointer1331@users.noreply.github.com>
2023-08-16 13:38:05 -0700

    Created Item form UI and validation function

--- GameShopManager/Forms/CreateItemForm.cs
+++ GameShopManager/Forms/CreateItemForm.cs
@@ -22,5 +22,40 @@ namespace GameShopManager.Forms
             this.navigationForm = navigationForm;
             InitializeComponent();
         }
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool ValidInput()
+        {
+            string itemName = ItemNameInput.Text.Trim();
+            string itemPrice = ItemPriceInput.Text.Trim();
+            string itemDescription = ItemDescriptionInput.Text.Trim();
+            string errorText = "";
+            if (string.IsNullOrEmpty(itemName))
+            {
+                errorText += "Item name is empty \n";
+            }
+            if (string.IsNullOrEmpty(itemDescription))
+            {
+                errorText += "Item quantity is empty \n";
+            }
+            if (string.IsNullOrEmpty(itemPrice))
+            {
+                errorText += "Item price is empty \n";
+            }
+            else if (!double.TryParse(itemPrice, out _))
+            {
+                errorText += "Item price must be a number \n";
+            }
+            if (errorText != "")
+            {
+                MessageBox.Show(errorText);
+                return false;
+            }
+            return true;
+        }
     }
 }
