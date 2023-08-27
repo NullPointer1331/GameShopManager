8d59f9edf59e8c3612ab01d1f6e9a483d157f2c6
TortillaWrappedParrot <119008633+TortillaWrappedParrot@users.noreply.github.com>
2023-08-27 03:00:14 -0700

    Added delete form functionality

--- GameShopManager/Forms/DeleteItemForm.cs
+++ GameShopManager/Forms/DeleteItemForm.cs
@@ -13,6 +13,8 @@ namespace GameShopManager.Forms
     public partial class DeleteItemForm : Form
     {
         NavigationForm navigationForm;
+        //Optimize loading
+        List<ItemObject> items;
         public DeleteItemForm()
         {
             InitializeComponent();
@@ -21,6 +23,48 @@ namespace GameShopManager.Forms
         {
             this.navigationForm = navigationForm;
             InitializeComponent();
+        }
+
+        private void refreshList()
+        {
+            item_listbox.Items.Clear();
+            foreach (ItemObject item in items)
+            {
+                item_listbox.Items.Add(item.ToString());
+            }
+        }
+
+        private void item_listbox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (item_listbox.SelectedItem != null)
+            {
+                delete_button.Enabled = true;
+            }
+            else
+            {
+                delete_button.Enabled = false;
+            }
+        }
+
+        private void delete_button_Click(object sender, EventArgs e)
+        {
+            if (item_listbox.SelectedItem != null)
+            {
+                DialogResult result = MessageBox.Show("Do you want to delete this item?", "Confirmation", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    ItemObject item = items[item_listbox.SelectedIndex];
+                    ItemDB.DeleteItem(item);
+                    items.Remove(item);
+                    refreshList();
+                }
+            }
+        }
+
+        private void DeleteItemForm_Load(object sender, EventArgs e)
+        {
+            items = ItemDB.GetAllItems();
+            refreshList();
         }
     }
 }
